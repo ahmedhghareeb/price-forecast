b580888a58aef97135f9a5024d6c16c569bdf933
Cameron Roach <camroach87@gmail.com>
2016-02-27 17:47:32 +1100

    Bugfix

--- tests/exploreData.py
+++ tests/exploreData.py
@@ -1,6 +1,6 @@
 # Title: Explore Data
 # Description: A little script to figure out how to use python and also what the data looks like.
-# Author: Cameron Roach
+__author__ = "Cameron Roach"
 
 #import csv as csv
 import pandas as pd
@@ -12,18 +12,18 @@ from datetime import datetime
 
 # Load data with pandas. Easier than csv module.
 genES = pd.read_csv('../data/HistData/gen_ES.csv', header=0, na_values='n.a.', parse_dates=True)
-genES.columns.values[0] = 'date' #TODO: This messes something up with info() and describe()
+genES.rename(columns = {'data':'date'}, inplace=True)
 genPT = pd.read_csv('../data/HistData/gen_PT.csv', header=0)
 pricePT = pd.read_csv('../data/HistData/price_PT.csv', header=0)
-
+weather = pd.read_csv('../data/HistWeather/weather_hist.csv')
 
 
 
 
 
 # Check import
-#print genES.info()
-#print genES.describe()
+print genES.info()
+print genES.describe()
 print genPT.info()
 print genPT.describe()
 print pricePT.info()
