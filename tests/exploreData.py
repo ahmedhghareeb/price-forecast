c55a56f4788fa76501821cc1f0f115da2924a373
Cameron Roach <camroach87@gmail.com>
2016-02-27 17:04:32 +1100

    Plots

--- tests/exploreData.py
+++ tests/exploreData.py
@@ -6,17 +6,24 @@
 import pandas as pd
 import numpy as np
 import pylab as P
+import matplotlib as plt
+from datetime import datetime
 
 
 # Load data with pandas. Easier than csv module.
-genES = pd.read_csv('../data/HistData/gen_ES.csv', header=0, na_values='n.a.')
+genES = pd.read_csv('../data/HistData/gen_ES.csv', header=0, na_values='n.a.', parse_dates=True)
+genES.columns.values[0] = 'date' #TODO: This messes something up with info() and describe()
 genPT = pd.read_csv('../data/HistData/gen_PT.csv', header=0)
 pricePT = pd.read_csv('../data/HistData/price_PT.csv', header=0)
 
 
+
+
+
+
 # Check import
-print genES.info()
-print genES.describe()
+#print genES.info()
+#print genES.describe()
 print genPT.info()
 print genPT.describe()
 print pricePT.info()
@@ -30,4 +37,8 @@ genES[genES['solar_photovoltaic'].isnull()]
 
 # Plots
 genES['solar_photovoltaic'].dropna().hist(bins=16, range=(0,80), alpha = .5)
+genES.plot(x = 'date', y ='solar_photovoltaic')
+#genES.boxplot()
+#genES.boxplot(column='solar_photovoltaic')
+
 P.show()
