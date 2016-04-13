2959a31272fc72b61062b7fa9d67b616dc162554
Cameron Roach <camroach87@gmail.com>
2016-04-13 17:41:04 +1000

    made n_data as a manual input at beginning of scripts

--- R/genHourModelsForecasts.R
+++ R/genHourModelsForecasts.R
@@ -21,10 +21,11 @@ require(caret)
 require(splines)
 
 subDate <- ymd("2016-04-12", tz="UTC")
+n_data <- "All"
 
 #### Run model with latest data ===============================================
 source("./R/testModels_HourModels_adjHrs.R")
-finalModel <- genHourPriceModel(subDate)
+finalModel <- genHourPriceModel(subDate, n_data)
 
 #### Load data ================================================================
 # Load yesterday's weather forecast to get hours 23 and 24 for submission date.
